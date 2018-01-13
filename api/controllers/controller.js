0292ae8a7e5ca0cb43b21b496fa136d86808062d
Anshul <agg_anshul@yahoo.com>
2018-01-13 14:57:42 -0800

    Creating a static landing page

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -2,12 +2,14 @@
 var mongoose = require('mongoose'),
   Temp = mongoose.model('Temp');
 
+var dir = 'C:/data/CS_Projects/LSD'
 
 exports.main = function(req, res) {
-	res.send("Hello World!");
+	res.sendFile('/index.html' , { root : dir});
 }
 exports.test = function(req, res) {
 	console.log("HEllo")
+	res.send("Hello")
 }
 exports.send_temp = function(req, res) {
   Temp.find().sort({date:1}).limit(1);
